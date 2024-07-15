eaf2bb76c8fd701765de139d2d02dbbad9d633ef
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-07-15 17:32:46 +0300

    done with the challenge

--- script/app.js
+++ script/app.js
@@ -1,48 +1,43 @@
-const url = 'https://week-3-challenge-sigma.vercel.app/films'
+const url = 'https://week-3-challenge-sigma.vercel.app/films';
 
-function createAnElement(element){
-    return document.createElement(element)
+function createAnElement(element) {
+    return document.createElement(element);
 }
 
 function addText(element, text) {
-    return (element.innerText = text)
-
-    
+    element.innerText = text;
 }
-function appendChild(child, parent) {
-    return parent.appendChild(child)
 
+function appendChild(child, parent) {
+    parent.appendChild(child);
 }
 
 function select(selector) {
-    return document.querySelector(selector)
-    
+    return document.querySelector(selector);
 }
 
 function listen(element, event, callback) {
-    return element.addEventListener(event, callback);
-
-    
+    element.addEventListener(event, callback);
 }
 
-function addAttribute(element,attribute, content) {
- return element.setAttribute(attribute, content)   
+function addAttribute(element, attribute, content) {
+    element.setAttribute(attribute, content);
 }
+
 listen(document, 'DOMContentLoaded', () => {
-    MoviesTitle(url);
+    MoviesTitle();
     MovieDetails(1);
 });
-
 
 function MoviesTitle() {
     fetch(url)
         .then(response => response.json())
         .then(movies => {
-            const movieList = select(".films");
-            movies.map(movie => {
-                const series = createAnElement("li");
+            const movieList = select('.films');
+            movies.forEach(movie => {
+                const series = createAnElement('li');
                 addText(series, movie.title);
-                listen(series, "click", () => MovieDetails(movie.id));
+                listen(series, 'click', () => MovieDetails(movie.id));
                 appendChild(series, movieList);
             });
         });
@@ -52,39 +47,36 @@ function MovieDetails(movieId) {
     fetch(`${url}/${movieId}`)
         .then(response => response.json())
         .then(movie => {
-            const moviePoster = select(".movie-poster");
-            addAttribute(moviePoster, "src", movie.poster);
+            const moviePoster = select('.movie-poster');
+            addAttribute(moviePoster, 'src', movie.poster);
 
-            const filmDes = select(".movie-description");
+            const filmDes = select('.movie-description');
             addText(filmDes, movie.description);
 
-            const filmTitle = select(".movie-title");
+            const filmTitle = select('.movie-title');
             addText(filmTitle, movie.title);
 
-            const runTime = select(".movie-runtime");
+            const runTime = select('.movie-runtime');
             addText(runTime, `Runtime: ${movie.runtime} minutes`);
 
-            const showTime = select(".movie-showtime");
+            const showTime = select('.movie-showtime');
             addText(showTime, `Showtime: ${movie.showtime}`);
             
-            const availTickets = select(".movie-available-tickets");
-            addText(availTickets, `Available Tickets: ${movie.capacity - movie.tickets_sold}`);
+            const availTickets = select('.movie-available-tickets');
+            let availableTickets = movie.capacity - movie.tickets_sold;
+            addText(availTickets, `Available Tickets: ${availableTickets}`);
 
-        }
-    )}
-
-    const buyTicket = select('.buy-ticket')
-    buyTicket.listen('click', function(e){
-        if(availTickets > 0){
-            select('.movie-available-tickets')
-           buyTicket.addText  = availTickets-1
-        }
-        else if((availTickets === 0)){
-            buyTicket.addText = 'Sold Out'
-            
-        }
-})
-    
-
-            
-           
+            const buyTicket = select('.buy-ticket');
+            listen(buyTicket, 'click', function() {
+                if (availableTickets > 0) {
+                    availableTickets -= 1;
+                    addText(availTickets, `Available Tickets: ${availableTickets}`);
+                    if (availableTickets === 0) {
+                        addText(buyTicket, 'Sold Out');
+                    }
+                } else {
+                    addText(buyTicket, 'Buy ticket');
+                }
+            });
+        });
+}
